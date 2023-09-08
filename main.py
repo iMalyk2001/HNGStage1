69c3953bfa4bd7728587d55848d5f2efbe45c56f
iMalyk2001 <imalyk.inc@gmail.com>
2023-09-08 19:10:15 +0100

    My first HNG Task

---
+++ main.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from fastapi import FastAPI
+import datetime
+from pydantic import BaseModel
+from json import JSONEncoder
+from http import HTTPStatus
+app = FastAPI()
+
+
+interns = {
+        "slack_name" : "JealousAide",
+        "track": "Backend",
+        "utc_time" : datetime.datetime.now(),
+        "github_file_url":"",
+        "github_repo_url":"",
+        "status_code": HTTPStatus.OK.value
+    }
+
+class DateTimeEncoder(JSONEncoder):
+        #Override the default method
+        def default(self, obj):
+            if isinstance(obj, (datetime.date, datetime.datetime)):
+                return obj.isoformat()
+
+
+@app.get("/api", status_code= 200)
+def intern_ship(slack_name:str="JealousAide", track:str="Backend"):
+    return interns
