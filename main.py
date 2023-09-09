69f3c115f24af36553afe802f8f10d9ebfd653a2
iMalyk2001 <imalyk.inc@gmail.com>
2023-09-09 13:26:54 +0100

    do

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ current_day= datetime.datetime.now()
 interns = {
         "slack_name" : "JealousAide",
         "current_day" : current_day.strftime('%A'),
-        "utc_time" : datetime.datetime.utcnow().strftime(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')),
+        "utc_time" : datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track": "backend",
         "github_file_url":"https://github.com/iMalyk2001/HNGStage1/blob/main/main.py",
         "github_repo_url":"https://github.com/iMalyk2001/HNGStage1",
         "status_code": HTTPStatus.OK.value
     }
 
-json_object = json.dumps(interns)
+
 
 
 @app.get("/api", status_code= 200)
 async def intern_ship(slack_name:str="JealousAide", track:str="backend"):
-    return json_object
+    return interns
