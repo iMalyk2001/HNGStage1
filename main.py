4569d2e7887414e0041a8b40de6b3360ad5c846f
iMalyk2001 <imalyk.inc@gmail.com>
2023-09-09 12:04:51 +0100

    mao

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
-import pytz
-from datetime import datetime
+import datetime
 from http import HTTPStatus
 app = FastAPI()
 
 
-time_wat = datetime.utcnow()
-
+time_wat = datetime.datetime.now()
 interns = {
         "slack_name" : "JealousAide",
         "current_day" : time_wat.strftime('%A'),
-        "utc_time" : datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "utc_time" : datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track": "backend",
         "github_file_url":"https://github.com/iMalyk2001/HNGStage1/blob/main/main.py",
         "github_repo_url":"https://github.com/iMalyk2001/HNGStage1",
