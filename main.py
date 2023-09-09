1ce1fa9e993ae8058f2a6208ba0304168d94d79c
iMalyk2001 <imalyk.inc@gmail.com>
2023-09-09 09:28:49 +0100

    mei

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from json import JSONEncoder
 from http import HTTPStatus
 app = FastAPI()
 
-deat = pytz.timezone('Europe/Paris')
+deat = pytz.timezone('Africa/Lagos')
 time_wat = datetime.now(deat)
 dt_str = time_wat.strftime('%Y-%m-%dT%H:%M:%SZ')
 
